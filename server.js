359643db1cac94a10bc76cc6e2eb6419da2fec08
Jacek1993 <jacek.rula1@gmail.com>
2018-11-24 11:32:39 +0100

    amendments

--- server.js
+++ server.js
@@ -12,28 +12,6 @@ var room=require('./routes/room');
 var client=require('./routes/client');
 
 
-
-
-
-
-/*
-
-czyli to co Rauch powiedział to na pewno trzeba używać schematów z nosql a nie sql ( rezerwacja zawiera się w dokumencie pokój), zmiana nazwy query przy poscie wiemy że jak mamy post to tworzymy więc createReservation jest co najmniej bez sensu
-jak już to może być reservation. Nie ma renderwania widoków więc czysta zabawa z postmanem
-Algorytm robienia projektu vol. 1.0:
--ogarnąć jak wygląda zagdnieżdżanie obiektów w mongodb oraz jakie zapytania się do tego stosuje
--stowrzyć dokumencje PORZĄDNIE przy czym mowa tutaj o każdej metodzie jakie ma parametry co robi co się z niej dostaje jakie wyjątki może rzucić
--stworzenie na podstawie powyższego opisu API bazy danych prawdopodobnie będzie kolekcja   Room oraz User możliwe że coś jeszcze
--a na końcu napisać powyższe metody REST w Node.js z uwzględnieniem wyjątków i wszystkich innych cudów jakie uzytkjownikowi moga przyjść do głowy
-
-
-*/
-
-
-
-
-
-
 var app=express();
 const port=process.env.PORT ||3000;
 app.use(bodyParser.json());
